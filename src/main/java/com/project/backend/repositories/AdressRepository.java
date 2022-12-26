1ce9f5ce3dc8681288db61b2a05a1a38abbf8e94
Axeloooo <axelshz@gmail.com>
2022-12-26 00:50:43 -0700

    Adress, costumer and order repositories created

---
+++ src/main/java/com/project/backend/repositories/AdressRepository.java
@@ -0,0 +1,9 @@
+package com.project.backend.repositories;
+
+import com.project.backend.models.Adress;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdressRepository extends JpaRepository<Adress, Long> {
+}
